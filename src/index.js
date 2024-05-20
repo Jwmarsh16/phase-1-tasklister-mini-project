71c3dc1679b7bdbc32b4b905b4c5c9792d17dca2
Jonathan Marshall <jwmarsh16@gmail.com>
2024-05-20 10:23:19 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const form = document.getElementById("create-task-form");
+  const tasksList = document.getElementById("tasks")
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+  const newTaskDescription = document.getElementById("new-task-description").value;
+  
+  
+  const newTaskItem = document.createElement("li")
+  newTaskItem.textContent = newTaskDescription;
+  
+  
+  tasksList.appendChild(newTaskItem);
+  
+  document.getElementById("new-task-description").value = "";
+  
+  const deleteButton = document.createElement('button');
+  
+  deleteButton.textContent = "X"
+  deleteButton.style.marginLeft = "10px"
+  deleteButton.style.padding = "2px 4px"
+  deleteButton.style.fontSize = "10px"
+
+  
+  deleteButton.addEventListener("click", () => {
+  tasksList.removeChild(newTaskItem);
+  
+  
+  });
+  
+  
+  newTaskItem.appendChild(deleteButton);
+  
+  
+    })
+  })
+  
